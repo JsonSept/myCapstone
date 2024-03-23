67272e3f4ce46a4be82560d60f6b35ffd8303b67
JsonSept <jasonseptember.za@gmail.com>
2024-03-23 04:51:59 +0200

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import bcrypt from 'bcrypt'
+import swal from 'sweetalert'
 import jwt from 'jsonwebtoken'
 import mysql2 from 'mysql2'
 import cookieParser from 'cookie-parser'
@@ -15,91 +16,53 @@ config()
 const PORT = process.env.PORT
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: 'mycapstone-27eaa.firebaseapp.com',
+    // origin: 'http://localhost:8080'
     // origin: 'http://capstone-social-app.onrender.com'
     // credentials:true
 }))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(
-    session({
-    secret: 'key that will sign cookie',
-    
-    resave: false,
-    saveUninitialized: false
+const posts = [
+{
+username: 'Jason',
+title: 'Post 1'
+},
+{
+    username: 'Jim',
+    title: 'Post 2'
+    }
+]
+app.get('/posts', authenticateToken, (req,res)=> {
+res.json(posts.filter(post => post.username === req.user.name))
+}),
+
+app.post('/login', (req, res)=> {
+// Authenticate User
+
+const username = req.body.username
+const user = { name: username }
+
+
+const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+res.json({ accessToken: accessToken })
 })
-);
+
+function authenticateToken(req,res,next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+    next();
+})
+};
 
 app.use('/products', productsRouter)
-app.use('/users', usersRouter)
-
-
-app.post('/register', async (req,res) => {
-    const { username , email , password} = req.body;
-
-    let user = await  UserModel.findOne({email});
-
-    if(user) {
-        return res.redirect('/register');
-    }
-
-    const hashedPsw = await bcrypt.hash(password, 12);
-    user = new UserModel({
-        username,
-        email,
-        password : hashedPsw
-    });
-    await user.save();
-
-    res.redirect('/login')
-});
-
-
-app.post('/users',(req,res)=>{
-    const {username,password} = req.body
-    bcrypt.hash(password,10, async (err,hash)=>{
-        if(err) throw err
-        await addUser(username,hash)
-        
-        res.send(await addUser(username,hash))
-    })
-    res.send({
-       msg: "You have registered successfully"
-    })
-})
-const auth = async (req,res,next) => {  //middleware (req,res,next())
-const {password,username} = req.body
-const hashedPassword = await checkUser(username )
-bcrypt.compare(password,hashedPassword, (err,result)=>{
-    if(err) throw err
-    if(result === true){
-        const {username} = req.body
-    const token = jwt.sign({username:username},
-    process.env.SECRET_KEY,{expiresIn:'1h'})
-    res.cookie('jwt',token,{httpOnly:false}) //true only backend user can access on frontend--- if it is set to true than only the backend user can set the
-    res.send({
-        token:token, //first one is the token ,second one is the value of the token
-        msg:'You have logged in!!! YAY!'
-    })
-    next()
-    }else{
-        res.send({msg:'The username or password is incorrect'})
-    }
-})
-}
-    app.post('/login',auth, (req,res)=>{
-        res.send({
-            msg:'You have logged in!!! YAY!'
-        })
-    })
-    app.delete('/logout',(req,res)=>{
-        res.clearCookie('jwt')
-        res.send({
-            msg:'You have logged out'
-        })
-    })
-
+app.use('/users', usersRouter),
 
 
 app.listen(PORT, ()  =>{
